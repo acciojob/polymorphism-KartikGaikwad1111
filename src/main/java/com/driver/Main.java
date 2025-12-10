60c7cacb11a335cd9e388cde90a8a1629c483a80
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-10 17:30:33 +0530

    Fixed structure and output for AccioJob JUnit tests

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,40 +1,36 @@
-package com.driver;
-
 public class Main {
-   static class Product {
-     // Task 3: Method with two int parameters
-        public int product(int x, int y) {
-            return x * y;
-        }
 
-        // Task 4: Overloaded method with three int parameters
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        // Task 5: Overloaded method with two double parameters
-        public double product(double x, double y) {
-            return x * y;
-        }
-    }
-
-    // Main function
     public static void main(String[] args) {
 
         // Task 2: Create object of Product
         Product p = new Product();
 
-        // Calling product(int, int)
-        int result1 = p.product(2, 3);
-        System.out.println("Product of 2 and 3 = " + result1);
+        // Task 3
+        System.out.println(p.product(2, 3));
 
-        // Calling product(int, int, int)
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Product of 2, 3 and 4 = " + result2);
+        // Task 4
+        System.out.println(p.product(2, 3, 4));
 
-        // Calling product(double, double)
-        double result3 = p.product(2.5, 3.5);
-        System.out.println("Product of 2.5 and 3.5 = " + result3);
+        // Task 5
+        System.out.println(p.product(2.5, 3.5));
     }
 }
 
+// Task 1: Product class
+class Product {
+
+    // Task 3
+    public int product(int x, int y) {
+        return x * y;
+    }
+
+    // Task 4
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+
+    // Task 5
+    public double product(double x, double y) {
+        return x * y;
+    }
+}
